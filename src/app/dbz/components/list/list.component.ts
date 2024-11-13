6c3b3f568586f712df3ffc23151cc12429f71bae
Angel Troncoso <angel.troncoso@ascendion.com>
2024-11-13 11:49:46 -0600

    completed

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,12 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {  Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
-  selector: 'app-dbz-list',
+  selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 
 export class ListComponent {
+
+  @Output()
+  public onDelete : EventEmitter<string> = new EventEmitter()
+
+
+  @Input()
+  public characterList : Character[] = [{
+    name: 'Trunks',
+    power: 50
+  }]
+
+  onDeleteCharacter(id? : string) : void {
+
+    if( !id) return;
+
+    this.onDelete.emit(id)
+  }
 
 }
